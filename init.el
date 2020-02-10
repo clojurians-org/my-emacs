709eb692539be18c6daecc50bc55d71ce7a25ee4
clojurians-org <larluo@clojurians.org>
2020-02-10 22:51:04 +0800

    init my emacs

---
+++ init.el
@@ -0,0 +1,35 @@
+(require 'package)
+
+(setq url-proxy-services
+   '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+     ("http" . "localhost:8118")
+     ("https" . "localhost:8118")))
+
+(setq package-check-signature nil)
+(setq package-archives
+      '(("gnu" . "https://elpa.emacs-china.org/gnu/")
+        ("melpa" . "https://elpa.emacs-china.org/melpa/")
+	))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package '(paredit dante cider lsp-java))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (cider paredit lsp-java))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
