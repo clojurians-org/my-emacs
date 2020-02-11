a3becfc77c36ae0ee969138eb6e688168a75623b
clojurians-org <larluo@clojurians.org>
2020-02-11 15:30:50 +0800

    add restclient

--- init.el
+++ init.el
@@ -16,7 +16,8 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(dolist (package '(paredit dante cider lsp-java))
+(dolist (package '(paredit restclient
+		   dante cider lsp-java))
   (unless (package-installed-p package)
     (package-install package)))
 
@@ -25,16 +26,5 @@
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (cider paredit lsp-java))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
