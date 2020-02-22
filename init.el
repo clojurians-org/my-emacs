8e61a5a545e1e79191eec4c28ec6237a8670231d
clojurians-org <larluo@clojurians.org>
2020-02-22 17:47:24 +0800

    add lombok

--- init.el
+++ init.el
@@ -18,8 +18,8 @@
   (package-refresh-contents))
 
 (dolist (package '(paredit restclient json-mode typescript-mode
-                   dap-mode lsp-java
-        		   dante cider 
+                   dap-mode lsp-java gradle-mode
+        		   dante cider
                    ))
   (unless (package-installed-p package)
     (package-install package)))
@@ -32,8 +32,7 @@
 (setq-default tab-width 4)
 (setq-default truncate-lines t)
 (custom-set-variables
- '(lsp-enable-on-type-formatting nil)
-)
+ '(lsp-enable-on-type-formatting nil))
 
 (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
@@ -44,14 +43,34 @@
 (add-hook 'typescript-mode-hook #'lsp)
 
 (add-hook 'conf-mode-hook (lambda () (display-line-numbers-mode t))) 
+
+(setq lsp-file-watch-ignored
+  '(".idea" ".ensime_cache" ".eunit" "node_modules"
+            ".git" ".hg" ".fslckout" "_FOSSIL_"
+            ".bzr" "_darcs" ".tox" ".svn" ".stack-work"
+            "build"))
 ;; -----------------
 ;; java mode
 ;; -----------------
 (require 'dap-java)
 (require 'lsp-java)
+
+;; nix-env -f nix-env -f ~/.nix-defexpr/channels/nixos-20.03 -iA lombok
+(setq path-to-lombok (car (file-expand-wildcards "/nix/store/*-lombok-*/share/java/lombok.jar") ) )
+(setq lsp-java-vmargs
+      `("-noverify"
+        "-Xmx1G"
+        "-XX:+UseG1GC"
+        "-XX:+UseStringDeduplication"
+        ,(concat "-javaagent:" path-to-lombok)
+        ,(concat "-Xbootclasspath/a:" path-to-lombok)))
+
 (add-hook 'java-mode-hook #'lsp)
 (add-hook 'java-mode-hook (lambda ()
                             (setq c-default-style "java")
                             (setq c-basic-offset 4)
                             (display-line-numbers-mode t)
+                            (gradle-mode 1)
                             ))
+
+
