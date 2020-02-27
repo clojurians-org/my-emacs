5d90b0fd115df209b1cf866a308f388470c3c8fc
clojurians-org <larluo@clojurians.org>
2020-02-27 22:04:08 +0800

    add home manager config

---
+++ home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "19.09";
+
+  home.packages = [
+    pkgs.coreutils
+    pkgs.tmux
+    pkgs.emacs
+    pkgs.vim
+    pkgs.cloc
+
+    pkgs.git
+    pkgs.cachix
+    pkgs.cabal-install
+    pkgs.obelisk
+    pkgs.yarn
+    pkgs.gradle
+    pkgs.clojure
+    pkgs.lombok
+    pkgs.clang-tools
+    pkgs.dhall
+    pkgs.jre
+    pkgs.python38
+    # pkgs.vue
+
+    pkgs.jq
+    pkgs.curl
+    pkgs.wget
+    pkgs.ws
+    pkgs.aria2
+
+    pkgs.postgresql
+    pkgs.minio
+    pkgs.neo4j
+
+    # pkgs.imagemagick
+    # pkgs.shadowsocks-libev
+  ];
+}
